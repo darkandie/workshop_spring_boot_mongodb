448ff6f63cbf6402f2036b45a1388473763e66b1
Carlos Oliveira <carlos.oliverhp@gmail.com>
2023-09-09 16:40:15 -0300

    Operação de instanciacao da base de dados

---
+++ src/main/java/com/carlos/workshop/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.carlos.workshop.repository;
+
+import com.carlos.workshop.domain.entities.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
---
+++ src/main/java/com/carlos/workshop/services/UserService.java
@@ -0,0 +1,19 @@
+package com.carlos.workshop.services;
+
+import com.carlos.workshop.domain.entities.User;
+import com.carlos.workshop.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll() {
+        return repo.findAll();
+    }
+}
