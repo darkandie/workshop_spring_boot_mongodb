99cd7b67e654256d3d187556da1c5d0ed31f4fe9
Carlos Oliveira <carlos.oliverhp@gmail.com>
2023-09-09 18:40:44 -0300

    inserção de usuário

--- src/main/java/com/carlos/workshop/services/UserService.java
+++ src/main/java/com/carlos/workshop/services/UserService.java
@@ -1,6 +1,7 @@
 package com.carlos.workshop.services;
 
 import com.carlos.workshop.domain.entities.User;
+import com.carlos.workshop.dto.UserDTO;
 import com.carlos.workshop.repository.UserRepository;
 import com.carlos.workshop.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
