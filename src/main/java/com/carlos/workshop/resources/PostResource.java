dd12fd0b04b09f3680ba4846f14dddf02f3abe63
Carlos Oliveira <carlos.oliverhp@gmail.com>
2023-09-10 17:27:29 -0300

    consulta simples com @Query

--- src/main/java/com/carlos/workshop/resources/PostResource.java
+++ src/main/java/com/carlos/workshop/resources/PostResource.java
@@ -1,13 +1,11 @@
 package com.carlos.workshop.resources;
 
 import com.carlos.workshop.domain.entities.Post;
+import com.carlos.workshop.resources.util.URL;
 import com.carlos.workshop.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +20,12 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping("/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
